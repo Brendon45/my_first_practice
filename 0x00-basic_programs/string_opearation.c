18437f628b13daa057185f0f664478d1725df64c
Brendon45 <jejebrendon722@gmail.com>
2023-11-30 14:51:25 -0800

    string opeartions

---
+++ 0x00-basic_programs/string_opearation.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+    char name[] = "Brendon";
+    char *ptr;
+    char *ptr1;
+    ptr = name;
+    ptr1 = &name;
+    
+
+    printf("Name: %s\n", name);
+    printf("Name: %c\n", *ptr);
+    printf("Name: %c\n", *ptr1);
+    printf("Name: %p\n", &name);
+    printf("Name: %p\n", ptr);
+    printf("Name: %p\n", ptr1);
+    return (0);
+}
