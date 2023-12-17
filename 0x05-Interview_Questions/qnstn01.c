0c2b1ea483cd97f548e397b96eccf3e124a49cb0
Brendon45 <jejebrendon722@gmail.com>
2023-12-17 01:19:57 -0800

    increment updated

---
+++ 0x05-Interview_Questions/qnstn01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* Question: What is the output of the following code
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int a = 5, b= 3;
+	int result = a * b + 1;
+
+	printf("%d\n", ++result);
+	return (0);
+}
