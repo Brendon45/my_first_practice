1c6cbeb12dd28f5feefa1fca3f01e7279f33623f
Brendon45 <jejebrendon722@gmail.com>
2023-12-17 02:18:15 -0800

    characters

---
+++ 0x05-Interview_Questions/qnstn05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* Question: What is the output of the following code
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	printf("c = %d\n", c = 255);
+	return (0);
+}
