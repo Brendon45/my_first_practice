cf2fc89c8db0ffedecaeae9789a9f891fad6a304
Brendon45 <jejebrendon722@gmail.com>
2023-12-17 02:22:12 -0800

    true or false

---
+++ 0x05-Interview_Questions/qnstn06.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Question: What is the output of the following code
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char c1 = 'a', c2 = 'b', c;
+
+	c = c1 + c2;
+	if (c > 'c')
+	{
+		printf("True\n");
+	}
+	else
+	{
+		printf("False\n");
+	}
+	return (0);
+}
