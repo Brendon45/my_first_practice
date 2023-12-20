f38488305daf99266cd33d642a009f3508329b14
Brendon45 <jejebrendon722@gmail.com>
2023-12-20 06:45:46 -0800

    program that creates a single linked list

---
+++ 0x06-linked_lists/linked_list2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Description - Program that creates a single linked list
+* Return: 0 (Success)
+*/
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+int main(void)
+{
+    struct node *head = NULL, *current = NULL;
+
+    head = malloc(sizeof(struct node));
+    head->data = 100;
+    head->link = NULL;
+
+    current = malloc(sizeof(struct node));
+    current->data = 200;
+    current->link = NULL;
+    head->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 300;
+    current->link = NULL;
+    head->link->link = current;
+
+    printf("%d\n", head->data);
+    printf("%d\n", head->link->data);
+    printf("%d\n", head->link->link->data);
+
+    free(head);
+    free(current);
+    
+    return 0;
+}
