8e7b7d3e54c6cd2b069622c065338f996d6a8c18
Brendon45 <jejebrendon722@gmail.com>
2023-12-20 06:39:49 -0800

    linked list fixed

---
+++ 0x06-linked_lists/linked_list1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Dscription: Creating a simple node in c
+* Creating a self referencial structure to represent node elements
+* Note that a node consist of 2 parts, data part and link part
+* Return: 0 (Success)
+*/
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+int main(void)
+{
+    struct node *head = NULL;
+
+    head = malloc(sizeof(struct node));
+
+    head->data = 90;
+    printf("%d\n", head->data);
+    printf("%p\n", head);
+
+    return (0);
+}
