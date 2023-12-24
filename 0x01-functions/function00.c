5e8033c88f13b8048ea96b5a0c688f86e14cfecf
Brendon45 <jejebrendon722@gmail.com>
2023-12-24 13:02:51 -0800

    tendai function

---
+++ 0x01-functions/function00.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* Description: 
+* Return 0 (Success)
+*/
+
+void myname(void);
+void reset_to_98(int *n);
+int main(void)
+{
+    int n;
+
+    n = 402;
+    
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    myname();
+    return (0);
+}
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
+void myname(void)
+{
+    printf("My name is Tendai.\n");
+}
