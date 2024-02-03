d6d1797c76d4baa7f43341ca40e1f9ca9d1675ec
Brendon45 <jejebrendon722@gmail.com>
2024-02-03 06:34:10 -0800

    character

---
+++ 0x02-Pointers/char_pointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* Description:
+* Return: 0 (Success)
+*/
+
+void modify_me(char *cc, char ccc){
+    *cc = 'o';
+    ccc = 'l';
+}
+
+int main(void)
+{
+    char c, res[20];
+    char *p;
+
+    c = 'H';
+    p = &c;
+    modify_me(p, c);
+    printf("Values: %c \n", c);
+
+    return (0);
+}
