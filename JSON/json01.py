3eec32942e5157a10a0f5a6a9986272c3b48aa4b
Brendon45 <jejebrendon722@gmail.com>
2024-03-04 13:47:02 -0800

    a JSON-formatted string

---
+++ JSON/json01.py
@@ -0,0 +1,17 @@
+import json
+
+# JSON -> JavaScript Object Notion
+# a standardized format commonly used to transfer data as text that can be sent over a network
+# JSON represents objects as name/value pairs, just like a Python dictionary.
+
+person_JSON = '{"name": "Brendon", "surname":"Jeje", "age":20}'
+
+print(type(person_JSON)) #the person_JSON is a string object
+
+person = json.loads(person_JSON) #converting JSON string into a valid python object, in particular a python dictionary
+
+print(type(person)) #the type of person is now a dict object
+
+## we can now perform normal dictionary operations such as method calls on person object
+
+print(person["name"])
