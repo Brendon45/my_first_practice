e4eb82477f7eec7ed1d5c1f1147d30ad7c1194e7
Brendon45 <jejebrendon722@gmail.com>
2024-03-18 10:49:58 +0200

    shifting using C

---
+++ shift/left_shift.c
@@ -0,0 +1,16 @@
+#include "shift.h"
+
+/**
+ * left_shift - Function left shifts bits of a number by n times
+ *
+ * @number: Number whose bits are to be shifted
+ * @times: Number of times to shift the bits
+ *
+ * Return: Number
+*/
+
+int left_shift(long number, int times)
+{
+	number <<= times;
+	return (number);
+}
