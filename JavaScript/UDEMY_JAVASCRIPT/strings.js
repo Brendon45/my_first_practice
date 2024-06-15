755e40c5ecb4d504972600877c008998f1f627d9
Brendon45 <jejebrendon722@gmail.com>
2024-06-15 20:52:57 +0200

    strings implementation

---
+++ JavaScript/UDEMY_JAVASCRIPT/strings.js
@@ -0,0 +1,29 @@
+// Declare a variable named greeting and assign it the string value "hello, world"
+let greeting = "hello, world";
+
+// Declare a variable named name and assign it the string value 'javaScript'
+let name = 'javaScript';
+
+// Log the value of greeting to the console
+console.log(greeting); // Output: "hello, world"
+
+// Log the value of name to the console
+console.log(name); // Output: "javaScript"
+
+
+// Declare variables firstName and lastName and assign them string values
+let firstName = "java";
+let lastName = "script";
+
+// Concatenate firstName and lastName with a space in between to create fullName
+let fullName = firstName + " " + lastName;
+
+// Log the value of fullName to the console
+console.log("Full name : " + fullName); // Output: "Full name : java script"
+
+
+// Declare a variable named message and assign it the string value "This is a beautiful day "
+let message = "This is a beautiful day ";
+
+// Log the length of the message string to the console
+console.log("Length of message : ", message.length); // Output: Length of message : 24
