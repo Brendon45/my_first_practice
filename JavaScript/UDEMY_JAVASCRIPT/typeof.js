e7fde7e68f8e41c9c57dd6319c62fe1e563f3e41
Brendon45 <jejebrendon722@gmail.com>
2024-06-15 20:44:37 +0200

    undefinedVariables

---
+++ JavaScript/UDEMY_JAVASCRIPT/typeof.js
@@ -0,0 +1,19 @@
+// Declare a variable without initializing it, so it is undefined
+let undefinedVariables;
+
+// Log the type of undefinedVariables to the console
+console.log("Type of undefinedVariables : ", typeof undefinedVariables); // Output: "undefined"
+
+// Define a function named exampleFunction
+function exampleFunction() {
+  return "I am a function!";
+}
+
+// Log the type of exampleFunction to the console
+console.log("Type of exampleFunction : ", typeof exampleFunction); // Output: "function"
+
+// Initialize a variable with the value null
+const nullValue = null;
+
+// Log the type of nullValue to the console
+console.log("Type of nullValue : ", typeof nullValue); // Output: "object"
