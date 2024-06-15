bc088d565d487d9b20eaff9802a2629b291c84ac
Brendon45 <jejebrendon722@gmail.com>
2024-06-15 19:15:32 +0200

    basic functin operations

---
+++ JavaScript/index_01.js
@@ -0,0 +1,12 @@
+let name = 'Brendon';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Brendon';
+let lastName = 'Jeje';
+console.log(firstName, lastName);
