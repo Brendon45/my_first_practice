0b1d9ad39f66fe1fcc9eff7844019a613229890c
Brendon45 <jejebrendon722@gmail.com>
2024-06-15 19:34:08 +0200

    function execution

---
+++ JavaScript/index_05.js
@@ -0,0 +1,8 @@
+// A function is a block of code that can be called and executed.
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+  }
+  
+  greet('Brendon', 'Jeje'); // Calling the function
+  greet('Given');
