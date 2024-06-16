21a4b78ee9ce20b15650c00f6938153e234af9d3
Brendon45 <jejebrendon722@gmail.com>
2024-06-16 20:48:57 +0200

    arithmetic operators implementation

---
+++ JavaScript/UDEMY_JAVASCRIPT/arithmetic.js
@@ -0,0 +1,17 @@
+/*Initialize variables x and y*/
+let x = 10;
+let y = 5;
+
+/*Perform arithmetic operations*/
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+let remainder = x % y;
+
+/*Log the results of the arithmetic operations to the console*/
+console.log(`SUM = ${sum}`);
+console.log(`DIFFERENCE = ${difference}`);
+console.log(`PRODUCT = ${product}`);
+console.log(`QUOTIENT = ${quotient}`);
+console.log(`REMAINDER = ${remainder}`);
