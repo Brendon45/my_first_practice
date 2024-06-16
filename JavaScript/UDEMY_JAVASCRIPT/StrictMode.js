28741dff6c651577a0095524a4fabef41864f950
Brendon45 <jejebrendon722@gmail.com>
2024-06-16 21:40:42 +0200

    strict mode implementation

--- JavaScript/UDEMY_JAVASCRIPT/StrictMode.js
+++ JavaScript/UDEMY_JAVASCRIPT/StrictMode.js
@@ -1,15 +1,15 @@
-// Enabling strict mode
+ /*Enabling strict mode*/
 "use strict";
 
-// Uncommenting the next line would throw a ReferenceError because the variable is not declared
-// undeclaredVariable = 42;
+/*Uncommenting the next line would throw a ReferenceError because the variable is not declared*/
+/*undeclaredVariable = 42;*/
 
 function silentErrorFunction() {
-    // Create an object with a property 'prop'
+    /*Create an object with a property 'prop'*/
     let obj = { prop: 42 };
 
-    // Using 'with' is not allowed in strict mode and will throw a SyntaxError
-    // The 'with' statement is commented out to avoid the error
+    /*Using 'with' is not allowed in strict mode and will throw a SyntaxError*/
+    /*The 'with' statement is commented out to avoid the error*/
     /*
     with (obj) {
         // Accessing 'prop' within the 'with' statement
@@ -18,5 +18,5 @@ function silentErrorFunction() {
     */
 }
 
-// Calling the function
+/*Calling the function*/
 silentErrorFunction();
