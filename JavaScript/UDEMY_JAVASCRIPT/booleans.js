f93949abf7fed410f4a7aeedb3ff1db49e882f5a
Brendon45 <jejebrendon722@gmail.com>
2024-06-16 21:55:33 +0200

    boolean expressions

---
+++ JavaScript/UDEMY_JAVASCRIPT/booleans.js
@@ -0,0 +1,21 @@
+// Define a constant age
+const age = 25;
+
+// Evaluate if age is greater than or equal to 18
+const isAdult = age >= 18;
+
+// Log the result of the boolean expression isAdult
+console.log("Is adult :", isAdult); // Output: Is adult : true
+
+
+// Define a constant temperature
+const temperature = 25;
+
+// Conditional statement to check temperature conditions
+if (temperature >= 30) {
+    console.log("it's a hot day! ");
+} else if (temperature >= 20) {
+    console.log("it's a pleasant day! ");
+} else {
+    console.log("it's a cold day! ");
+}
