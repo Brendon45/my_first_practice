6c15cca872092e4858cf6fb53f05f49988fd5bee
Brendon45 <jejebrendon722@gmail.com>
2024-06-17 09:56:28 +0200

    constructor property

--- JavaScript/Obect_Oriented_Programming/constructor-property.js
+++ JavaScript/Obect_Oriented_Programming/constructor-property.js
@@ -18,7 +18,7 @@
  * - The constructor property is available on all sets
  */
 
-// Creating instances of built-in objects
+/*Creating instances of built-in objects*/
 let str = new String("Hello"); // Creating a String object
 let bool = new Boolean(true); // Creating a Boolean object
 let num = new Number(123); // Creating a Number object
@@ -27,9 +27,9 @@ console.log(str.constructor); // Output: [Function: String]
 console.log(bool.constructor); // Output: [Function: Boolean]
 console.log(num.constructor); // Output: [Function: Number]
 
-// In JavaScript, functions are objects, and they have a constructor property
+/*In JavaScript, functions are objects, and they have a constructor property*/
 
-// Constructor Function
+/*Constructor Function*/
 function Circle(radius) {
     this.radius = radius; // Property
     this.draw = function() { // Method
@@ -39,26 +39,26 @@ function Circle(radius) {
 
 console.log(Circle.constructor); // Output: [Function: Function]
 
-// Creating a Circle instance using the Circle constructor function
+/*Creating a Circle instance using the Circle constructor function*/
 const circle1 = new Circle(1);
 console.log(circle1.constructor); // Output: [Function: Circle]
 
-// Using the Function constructor to create a function
+/*Using the Function constructor to create a function*/
 const Circle1 = new Function('radius', `
     this.radius = radius;
     this.draw = function() {
         console.log('draw');
     }
-`); // new Function() is not recommended for creating functions
+`); /*new Function() is not recommended for creating functions*/
 
-// Creating a Circle instance using the Function constructor
+/*Creating a Circle instance using the Function constructor*/
 const circle2 = new Circle1(1);
 console.log(circle2.constructor); // Output: [Function: Function]
 
-// Calling a function with a specified 'this' value using call() and apply()
+/*Calling a function with a specified 'this' value using call() and apply()*/
 Circle.call({}, 1); // {} is the value of this
 Circle.apply({}, [1]); // {} is the value of this
 
-// Creating another Circle instance
+/*Creating another Circle instance*/
 const another = new Circle(1);
 console.log(another.constructor); // Output: [Function: Circle]
