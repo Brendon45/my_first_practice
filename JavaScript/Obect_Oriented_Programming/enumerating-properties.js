4e4ffed7a0d3454c3394fd8826c0a2a22e5a9be9
Brendon45 <jejebrendon722@gmail.com>
2024-06-17 09:21:07 +0200

    enumerating-properties

--- JavaScript/Obect_Oriented_Programming/enumerating-properties.js
+++ JavaScript/Obect_Oriented_Programming/enumerating-properties.js
@@ -8,7 +8,7 @@
  * This code demonstrates various ways to enumerate properties of an object in JavaScript.
  */
 
-// Constructor function to create a Circle object
+/*Constructor function to create a Circle object*/
 function Circle(radius) {
     this.radius = radius; // Define a property 'radius'
     this.draw = function() { // Define a method 'draw'
@@ -16,30 +16,30 @@ function Circle(radius) {
     }
 }
 
-// Create a new Circle object
+/*Create a new Circle object*/
 const circle = new Circle(10);
 
 // Sometimes we need to iterate over or enumerate the properties in an object.
 // We can do that using a for-in loop.
 
-// Using for-in loop to enumerate all properties (including methods) in the object
+/*Using for-in loop to enumerate all properties (including methods) in the object*/
 for (let key in circle) {
-    // This will print all properties and methods
+    /*This will print all properties and methods*/
     console.log(key, circle[key]); 
 
-    // Only get the properties that are not functions
+    /*Only get the properties that are not functions*/
     if (typeof circle[key] !== 'function') { 
         console.log('Non-function property:', key, circle[key]);
     }
 }
 
 // To get all the keys in an object, we can use the Object.keys() method.
-// Object.keys() returns an array of a given object's own enumerable property names.
+/*Object.keys() returns an array of a given object's own enumerable property names*/
 const keys = Object.keys(circle); // Get all the keys and store them in an array
 console.log('Keys using Object.keys():', keys); // Prints ['radius', 'draw']
 
 // Sometimes you want to know if an object has a given property.
-// We can do that using the in operator.
+/*We can do that using the in operator*/
 if ('radius' in circle) {
     console.log('Circle has a radius');
 }
